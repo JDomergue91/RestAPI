9959ee1125fc6b796587aa32d5183f1786f3b590
Jdomergue91 <domerguejonathan@gmail.com>
2024-03-18 11:34:28 +0100

    Server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello Typesscript!");
+import express from 'express';
+import http from 'http';
+import bodyParcer from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+//console.log("Hello Typesscript!");
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParcer.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log('Serveur running on http://localhost:8080/');
+});
