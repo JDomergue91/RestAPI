01eba6a9029b06c10d82a37941a5b15af23ae874
Jdomergue91 <domerguejonathan@gmail.com>
2024-06-02 17:25:31 +0200

    "creation user model + set up DB"

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParcer from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 //console.log("Hello Typesscript!");
 
@@ -22,3 +23,9 @@ const server = http.createServer(app);
 server.listen(8080, () => {
     console.log('Serveur running on http://localhost:8080/');
 });
+
+const MONGO_URL = 'mongodb+srv://domerguejonathan91:cK0vPOg39VWg0kDm@cluster0.hjx6dcz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
