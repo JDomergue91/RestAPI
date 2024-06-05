890819c2cac08d9648ca8dcee74340953a427846
Jdomergue91 <domerguejonathan@gmail.com>
2024-06-05 16:06:33 +0200

    "register controller"

--- src/DB/users.ts
+++ src/DB/users.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
-    authentification: {
+    authentication: {
         password: { type: String, required: true, select: false },
         salt: { type: String, select: false },
         sessionToken: { type: String, select: false},
@@ -15,7 +15,7 @@ export const UserModel = mongoose.model('User', UserSchema);
 export const getUsers = () => UserModel.find();
 export const getUserByEmail = (email: string) => UserModel.findOne({ email });
 export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({
-    'authentification.sessionsToken': sessionToken,
+    'authentication.sessionsToken': sessionToken,
 });
 export const getUserById = (id: string) => UserModel.findById(id);
 export const createUser = (values: Record<string, any>) => new UserModel (values)
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+import router from './router';
+
 //console.log("Hello Typesscript!");
 
 const app = express();
@@ -29,3 +31,4 @@ mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
 
+app.use('/', router());
